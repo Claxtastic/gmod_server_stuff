c1d6fbb20399d7e762775638834da8fcc8b785d6
claxtont <claxtons4@gmail.com>
2020-04-03 00:24:01 -0500

    Fixed folder structure in merge.py

--- merge.py
+++ merge.py
@@ -8,42 +8,46 @@ def merge(destination):
 	shutil.copy2("start-server.bat", destination)
 
 	print("\nMerging '/cfg/' ...\n")
-	for file in os.listdir("cfg"):
-		cfg_src = os.path.join("cfg", file)
+	for file in os.listdir("garrysmod/cfg"):
+		cfg_src = os.path.join("garrysmod", "cfg", file)
+		print(cfg_src)
 		cfg_dest = os.path.join(destination, "cfg")
 		print("\tCopying:\t %s" % file)
 		shutil.copy2(cfg_src, cfg_dest)
 
 	print("\nMerging '/gamemodes/terrortown/entities/weapons/' ...\n")
-	for file in os.listdir("gamemodes/terrortown/entities/weapons"):
-		weapons_src = os.path.join("gamemodes", "terrortown", "entities", "weapons", file)
+	for file in os.listdir("garrysmod/gamemodes/terrortown/entities/weapons"):
+		weapons_src = os.path.join("garrysmod", "gamemodes", "terrortown", "entities", "weapons", file)
 		weapons_dest = os.path.join(destination, "gamemodes", "terrortown", "entities", "weapons")
 		print("\tCopying:\t %s" % file)
 		shutil.copy2(weapons_src, weapons_dest)
 
 	print("\nMerging '/lua/weapons/' ...\n")
-	for file in os.listdir("lua/weapons"):
-		lua_src = os.path.join("lua", "weapons", file)
+	for file in os.listdir("garrysmod/lua/weapons"):
+		lua_src = os.path.join("garrysmod", "lua", "weapons", file)
 		lua_dest = os.path.join(destination, "lua", "weapons")
 		print("\tCopying:\t %s" % file)
 		shutil.copy2(lua_src, lua_dest)
 
 	print("\nMerging '/sound/endroundmusic/' ...\n")
 	# these dirs are not created until first server start, so let's create them now
-	os.makedirs(os.path.join(destination, "sound", "endroundmusic", "innocent"))
-	os.makedirs(os.path.join(destination, "sound", "endroundmusic", "traitor"))
-	os.makedirs(os.path.join(destination, "sound", "endroundmusic", "timeout"))
+	if not os.path.exists(os.path.join(destination, "sound", "endroundmusic", "innocent")):
+		os.makedirs(os.path.join(destination, "sound", "endroundmusic", "innocent"))
+	if not os.path.exists(os.path.join(destination, "sound", "endroundmusic", "traitor")):
+		os.makedirs(os.path.join(destination, "sound", "endroundmusic", "traitor"))
+	if not os.path.exists(os.path.join(destination, "sound", "endroundmusic", "timeout")):
+		os.makedirs(os.path.join(destination, "sound", "endroundmusic", "timeout"))
 
-	for file in os.listdir("sound/endroundmusic/innocent"):
+	for file in os.listdir("garrysmod/sound/endroundmusic/innocent"):
 		# innocent music
-		innocent_src = os.path.join("sound", "endroundmusic", "innocent", file)
+		innocent_src = os.path.join("garrysmod", "sound", "endroundmusic", "innocent", file)
 		innocent_dest = os.path.join(destination, "sound", "endroundmusic", "innocent")
 		print("\tCopying:\t %s" % file)
 		shutil.copy2(innocent_src, innocent_dest)
 
-	for file in os.listdir("sound/endroundmusic/traitor"):
+	for file in os.listdir("garrysmod/sound/endroundmusic/traitor"):
 		# traitor music
-		traitor_src = os.path.join("sound", "endroundmusic", "traitor", file)
+		traitor_src = os.path.join("garrysmod", "sound", "endroundmusic", "traitor", file)
 		traitor_dest = os.path.join(destination, "sound", "endroundmusic", "traitor")
 		print("\tCopying:\t %s" % file)
 		shutil.copy2(traitor_src, traitor_dest)
