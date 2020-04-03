91519a00dc78000c59fc8e2396a62a22c55cdd7d
claxtont <claxtons4@gmail.com>
2020-04-02 22:44:37 -0500

    Added start server batch

---
+++ start-server.bat
@@ -0,0 +1,4 @@
+@echo off
+start srcds.exe -console -game "garrysmod" -port 27015 +exec "server.cfg" +host_workshop_collection 547692215
++gamemode terrortown +map 
+ttt_rooftops_2016 +maxplayers 20
