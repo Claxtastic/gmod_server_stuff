622212236262cb0d1d676a6bac635de44be50c4b
claxtont <claxtons4@gmail.com>
2020-04-03 00:14:14 -0500

    Added auto merge python script

---
+++ merge.py
@@ -0,0 +1,62 @@
+import sys
+import os
+import shutil
+
+def merge(destination):
+	print("Moving start batch file to root ...")
+	print("\tCopying:\t start-server.bat")
+	shutil.copy2("start-server.bat", destination)
+
+	print("\nMerging '/cfg/' ...\n")
+	for file in os.listdir("cfg"):
+		cfg_src = os.path.join("cfg", file)
+		cfg_dest = os.path.join(destination, "cfg")
+		print("\tCopying:\t %s" % file)
+		shutil.copy2(cfg_src, cfg_dest)
+
+	print("\nMerging '/gamemodes/terrortown/entities/weapons/' ...\n")
+	for file in os.listdir("gamemodes/terrortown/entities/weapons"):
+		weapons_src = os.path.join("gamemodes", "terrortown", "entities", "weapons", file)
+		weapons_dest = os.path.join(destination, "gamemodes", "terrortown", "entities", "weapons")
+		print("\tCopying:\t %s" % file)
+		shutil.copy2(weapons_src, weapons_dest)
+
+	print("\nMerging '/lua/weapons/' ...\n")
+	for file in os.listdir("lua/weapons"):
+		lua_src = os.path.join("lua", "weapons", file)
+		lua_dest = os.path.join(destination, "lua", "weapons")
+		print("\tCopying:\t %s" % file)
+		shutil.copy2(lua_src, lua_dest)
+
+	print("\nMerging '/sound/endroundmusic/' ...\n")
+	# these dirs are not created until first server start, so let's create them now
+	os.makedirs(os.path.join(destination, "sound", "endroundmusic", "innocent"))
+	os.makedirs(os.path.join(destination, "sound", "endroundmusic", "traitor"))
+	os.makedirs(os.path.join(destination, "sound", "endroundmusic", "timeout"))
+
+	for file in os.listdir("sound/endroundmusic/innocent"):
+		# innocent music
+		innocent_src = os.path.join("sound", "endroundmusic", "innocent", file)
+		innocent_dest = os.path.join(destination, "sound", "endroundmusic", "innocent")
+		print("\tCopying:\t %s" % file)
+		shutil.copy2(innocent_src, innocent_dest)
+
+	for file in os.listdir("sound/endroundmusic/traitor"):
+		# traitor music
+		traitor_src = os.path.join("sound", "endroundmusic", "traitor", file)
+		traitor_dest = os.path.join(destination, "sound", "endroundmusic", "traitor")
+		print("\tCopying:\t %s" % file)
+		shutil.copy2(traitor_src, traitor_dest)
+
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print("Must provide path to root of server in command line arguments")
+		sys.exit()
+
+	try:
+		merge(sys.argv[1])
+	except Exception as e:
+		print("\nERROR! Make sure the vanilla server files downloaded properly from steamcmd and all directories were created and this script is in the content directory")
+		print("More information:")
+		print(e)
