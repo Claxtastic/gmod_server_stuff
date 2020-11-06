c1a1e3abb80a0c0dbfaccac76d2712e5826ee426
claxtastic <claxtons4@gmail.com>
2020-11-05 22:45:48 -0600

    Cleanupp

--- start-server.bat
+++ start-server.bat
@@ -1,3 +1,3 @@
 @echo off
 start srcds.exe -console -game "garrysmod" -port 27015 +exec "server.cfg" +host_workshop_collection 547692215
-+maxplayers 26 +gamemode terrortown +map ttt_rooftops_2016 
++maxplayers 26 +gamemode terrortown +map ttt_rooftops_2016
